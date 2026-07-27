48c3f1ff03a8ee6f0f991d3d1eafbd21fec2e108
songfox00 <songfox00@gmail.com>
2026-07-27 15:37:51 +0900

    init

---
+++ src/main/java/com/ll/wiseSaying/Main.java
@@ -0,0 +1,8 @@
+package com.ll.wiseSaying;
+
+public class Main {
+    static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+}
---
+++ src/main/java/com/ll/wiseSaying/WiseSaying.java
@@ -0,0 +1,37 @@
+package com.ll.wiseSaying;
+
+public class WiseSaying {
+    private int id;
+    private String author;
+    private String content;
+
+    public WiseSaying(int id, String author, String content) {
+        this.id = id;
+        this.author = author;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
